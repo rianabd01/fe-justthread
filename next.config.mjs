7d26f3a1878e4ce1b2058f2dab6619e7d242c275
rianabdillah <44765565+rianabd01@users.noreply.github.com>
2025-11-23 16:51:53 +0700

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //   output: "export",
-  distDir: "./dist",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   typescript: {
     ignoreBuildErrors: true, // remove if ts was proper
